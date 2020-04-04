e41d69ba6153983a592bd19705bf9254f9e2501e
Lorenzo León <lorenzoln@gmail.com>
2020-04-03 23:58:20 -0600

    jdjd

---
+++ knit_webpage.R
@@ -0,0 +1,20 @@
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/index.Rmd", 
+                  output_format = "pdf_document",  output_file = "index")
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/index.Rmd", 
+                  output_format = "html_document", output_file = "index")
+
+
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
+                  output_format = "pdf_document",  output_file = "index", params = "ask")
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
+                  output_format = "html_document", output_file = "index", params = "ask")
+
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
+                  output_format = "pdf_document",  output_file = "index", params = "ask")
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
+                  output_format = "html_document", output_file = "index", params = "ask")
+
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
+                  output_format = "pdf_document",  output_file = "index", params = "ask")
+rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
+                  output_format = "html_document", output_file = "index", params = "ask")
