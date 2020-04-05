3a520067f9b119abef2bd7339dd9d355fc9f72af
Lorenzo León <lorenzoln@gmail.com>
2020-04-04 22:25:56 -0600

    recheck

---
+++ knit_webpage.R
@@ -0,0 +1,22 @@
+setwd("~/Desktop/extra/covid19_mex_Reportes/")
+
+rmarkdown::render(input = "AnalisisParametria.Rmd", 
+                  output_format = "pdf_document",  output_file = "AnalisisParametria")
+rmarkdown::render(input = "AnalisisParametria.Rmd", 
+                  output_format = "html_document", output_file = "index")
+
+rmarkdown::render(input = "RedesSociales.Rmd", 
+                  output_format = "pdf_document",  output_file = "RedesSociales")
+rmarkdown::render(input = "RedesSociales.Rmd", 
+                  output_format = "html_document", output_file = "RedesSociales")
+
+
+rmarkdown::render(input = "OpinionPublica.Rmd", 
+                  output_format = "pdf_document",  output_file = "OpinionPublica")
+rmarkdown::render(input = "OpinionPublica.Rmd", 
+                  output_format = "html_document", output_file = "OpinionPublica")
+
+rmarkdown::render(input = "DatosSalud.Rmd", 
+                  output_format = "html_document",
+                  output_file = "DatosSalud", params = "ask")
+
