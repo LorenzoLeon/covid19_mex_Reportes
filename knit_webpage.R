8afe64208be6ca492835a645d993f4e44bdbaf1b
Lorenzo León <lorenzoln@gmail.com>
2020-04-04 19:22:04 -0600

    separados

--- knit_webpage.R
+++ knit_webpage.R
@@ -1,20 +1,23 @@
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/index.Rmd", 
-                  output_format = "pdf_document",  output_file = "index")
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/index.Rmd", 
-                  output_format = "html_document", output_file = "index")
+setwd("~/Desktop/extra/covid19_mex_Reportes/")
+
+rmarkdown::render(input = "AnalisisParametria.Rmd", 
+                  output_format = "pdf_document",  output_file = "AnalisisParametria.pdf")
+rmarkdown::render(input = "AnalisisParametria.Rmd", 
+                  output_format = "html_document", output_file = "index.html")
+
+rmarkdown::render(input = "RedesSociales.Rmd", 
+                  output_format = "pdf_document",  output_file = "RedesSociales")
+rmarkdown::render(input = "RedesSociales.Rmd", 
+                  output_format = "html_document", output_file = "RedesSociales")
 
 
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
-                  output_format = "pdf_document",  output_file = "index", params = "ask")
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
-                  output_format = "html_document", output_file = "index", params = "ask")
+rmarkdown::render(input = "OpinionPublica.Rmd", 
+                  output_format = "pdf_document",  output_file = "OpinionPublica")
+rmarkdown::render(input = "OpinionPublica.Rmd", 
+                  output_format = "html_document", output_file = "OpinionPublica")
 
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
-                  output_format = "pdf_document",  output_file = "index", params = "ask")
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
-                  output_format = "html_document", output_file = "index", params = "ask")
+rmarkdown::render(input = "DatosSalud.Rmd", 
+                  output_format = "pdf_document",  output_file = "DatosSalud")
+rmarkdown::render(input = "DatosSalud.Rmd", 
+                  output_format = "html_document", output_file = "DatosSalud")
 
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
-                  output_format = "pdf_document",  output_file = "index", params = "ask")
-rmarkdown::render(input = "Desktop/extra/covid19_mex_Reportes/reportes.Rmd", 
-                  output_format = "html_document", output_file = "index", params = "ask")
