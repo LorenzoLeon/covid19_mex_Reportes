e28aef4b8ab8e9e81d370edb954a21ded1c46c89
Lorenzo León <lorenzoln@gmail.com>
2020-04-06 19:42:26 -0500

    error en clearing vars

--- knit_webpage.R
+++ knit_webpage.R
@@ -12,11 +12,15 @@ rmarkdown::render(input = "OpinionPublica.Rmd",
 ### Build Webpage
 rmarkdown::render(input = "AnalisisParametria.Rmd", 
                   output_format = "html_document", output_file = "index")
+rm(list = ls(all.names = TRUE))
 rmarkdown::render(input = "RedesSociales.Rmd", 
                   output_format = "html_document", output_file = "RedesSociales")
+rm(list = ls(all.names = TRUE))
 rmarkdown::render(input = "OpinionPublica.Rmd", 
                   output_format = "html_document", output_file = "OpinionPublica")
+rm(list = ls(all.names = TRUE))
 rmarkdown::render(input = "DatosSalud.Rmd", 
                   output_format = "html_document",
                   output_file = "DatosSalud")#, params = "ask")
+rm(list = ls(all.names = TRUE))
 
